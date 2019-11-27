91a96518263752084cc5587c57c71217dafd6633
sangzi1006 <39059259+sangzi1006@users.noreply.github.com>
2019-11-27 15:27:50 +0100

    first commit

--- src/test/java/zi/com/reactor/MonoAndFluxDemoTest.java
+++ src/test/java/zi/com/reactor/MonoAndFluxDemoTest.java
@@ -8,23 +8,7 @@ public class MonoAndFluxDemoTest {
     private int a = 1000;
 
     @Test
-    public void testJustDeferFromCallableScenario1(){
-
-        Mono monoJust = Mono.just(a);
-        Mono monoFromCallable = Mono.fromCallable(() -> a);
-        Mono monoDefer = Mono.defer(() -> Mono.just(a));
-        Mono monoDeferFromCallable = Mono.defer(() -> Mono.fromCallable(() -> a));
-
-        a = 2000;
-
-        monoJust.subscribe(System.out::println);
-        monoFromCallable.subscribe(System.out::println);
-        monoDefer.subscribe(System.out::println);
-        monoDeferFromCallable.subscribe(System.out::println);
-    }
-
-    @Test
-    public void testJustDeferFromCallableScenario2(){
+    public void testJustDeferFromCallable(){
 
         Mono monoJust = Mono.just(generate("monoJust"));
         Mono monoFromCallable = Mono.fromCallable(() -> generate("monoFromCallable"));
